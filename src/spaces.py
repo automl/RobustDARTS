3a87007c29464fb5f8bffbdd8952c99889c742d1
Arber Zela <zelaa@informatik.uni-freiburg.de>
2019-07-27 15:45:40 +0200

    added space s5

--- src/spaces.py
+++ src/spaces.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 
 
+
+
 primitives_1 = OrderedDict([('primitives_normal', [['skip_connect',
                                                     'dil_conv_3x3'],
                                                     ['skip_connect',
@@ -75,10 +77,25 @@ primitives_4 = OrderedDict([('primitives_normal', 14 * [['noise',
                             ('primitives_reduct', 14 * [['noise',
                                                          'sep_conv_3x3']])])
 
+PRIMITIVES = [
+    #'none',
+    'max_pool_3x3',
+    'avg_pool_3x3',
+    'skip_connect',
+    'sep_conv_3x3',
+    'sep_conv_5x5',
+    'dil_conv_3x3',
+    'dil_conv_5x5'
+]
+
+primitives_5 = OrderedDict([('primitives_normal', 14 * [PRIMITIVES]),
+                            ('primitives_reduct', 14 * [PRIMITIVES])])
+
 spaces_dict = {
     's1': primitives_1,
     's2': primitives_2,
     's3': primitives_3,
     's4': primitives_4,
+    's5': primitives_5, # original DARTS space
 }
 
