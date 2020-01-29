f5a947377803caa808cccc2d06b87c4e6c756c4a
rx5935 <yejinseo@dyn3177-102.wlan.ic.ac.uk>
2020-01-29 10:04:28 +0000

    search script fix attempt

--- scripts/DARTS_search.sh
+++ scripts/DARTS_search.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 #
 # submit to the right queue
-#SBATCH -p meta_gpu-ti
 #SBATCH --gres gpu:1
-#SBATCH -a 1-3
-#SBATCH -J DARTS_grid
-#
-# the execution will use the current directory for execution (important for relative paths)
-#SBATCH -D .
-#
-# redirect the output/error to some files
-#SBATCH -o ./experiments/cluster_logs/%A_%a.o
-#SBATCH -e ./experiments/cluster_logs/%A_%a.e
-#
-#
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<your_username>
+
 
 source activate pytorch-0.3.1-cu8-py36
 python src/search/train_search.py --unrolled --job_id $SLURM_ARRAY_JOB_ID --task_id $SLURM_ARRAY_TASK_ID --seed $SLURM_ARRAY_TASK_ID --cutout --report_freq_hessian 2 --space $1 --dataset $2 --drop_path_prob $3 --weight_decay $4
