d2af813cc5c23d42249a8718439c52877fc2678a
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-04-09 09:01:15 +0900

    activating python virtual environment in dr detection script

--- scripts/start_dr_detection_search.sh
+++ scripts/start_dr_detection_search.sh
@@ -3,6 +3,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ys3316
 
+export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
+source activate
+source /vol/cuda/10.0.130/setup.sh
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
 python src/search/train_search.py --unrolled --train_portion 0.1 --cutout --report_freq_hessian 2 --space s1 --dataset dr-detection --drop_path_prob 0.2 --weight_decay 0.0121
 
 #datasets="dr-detection"
