9b55b96df3aa9694a72d8660fad6004fd600d238
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-04-25 18:44:18 +0900

    updated batch size

--- scripts/start_dr_detection_eval.sh
+++ scripts/start_dr_detection_eval.sh
@@ -9,6 +9,6 @@ source /vol/cuda/10.0.130/setup.sh
 TERM=vt100
 /usr/bin/nvidia-smi
 uptime
-python src/evaluation/train.py --cutout --auxiliary --seed 1 --space s1 --dataset dr-detection --search_dp 0.0 --search_wd 0.0009 --layers 14
+python src/evaluation/train.py --cutout --auxiliary --seed 1 --space s1 --dataset dr-detection --search_dp 0.0 --search_wd 0.0009 --layers 14 --batch_size 3
 
 
